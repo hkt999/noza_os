6cf14b65afc6b8ad1f37d26c3243014eab9e7495
Kevin Huang <hkt999@gmail.com>
2023-03-18 17:56:23 +0800

    change the data pass from kernel to user mode

--- user/noza_os_demo.c
+++ user/noza_os_demo.c
@@ -9,19 +9,26 @@ void test_task(void *param)
     const char *str = (const char *) param;
     while (--do_count>0) {
         printf("%d, %s\n", do_count, str);
-        printf("=============================================\n");
-        thread_yield();
-        usleep(1000000);
+        noza_thread_yield();
+        noza_thread_sleep(1000);
     }
 }
 
 void init_task(void *param)
 {
     uint32_t th1, th2, th3, th4;
+    noza_thread_sleep(2000);
 
-    th1 = thread_create(test_task, "task 1", 0);
-    th2 = thread_create(test_task, "task 2", 1);
-    th2 = thread_create(test_task, "task 3", 2);
-    th3 = thread_create(test_task, "task 4", 3);
-    test_task("task 5");
+    th1 = noza_thread_create(test_task, "task 1", 0);
+    noza_thread_sleep(100);
+    th2 = noza_thread_create(test_task, "task 2", 1);
+    noza_thread_sleep(100);
+    th3 = noza_thread_create(test_task, "task 3", 2);
+    noza_thread_sleep(100);
+    th4 = noza_thread_create(test_task, "task 4", 3);
+    noza_thread_sleep(100);
+    while (1) {
+        printf("%d, %d, %d, %d\n", th1, th2, th3, th4);
+        noza_thread_sleep(700);
+    }
 }
--- user/syslib.c
+++ user/syslib.c
@@ -1,27 +1,45 @@
 #include "syslib.h"
 #include "kernel/syscall.h"
 
-extern void asm_noza_syscall(uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3);
-uint32_t noza_syscall(uint32_t r0, uint32_t r1, uint32_t r2)
-{
-	uint32_t ret_addr = 0;
-	asm_noza_syscall(r0, r1, r2, (uint32_t)&ret_addr); // the value will be filled into ret_addr
-
-	return ret_addr; // so just return
-}
+extern int noza_syscall(uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3);
 
 // exported lib functions
-int32_t usleep(uint32_t us)
+int noza_thread_sleep(uint32_t ms)
 {
-	return noza_syscall(NSC_SLEEP, us/1000, 0);
+	return noza_syscall(NSC_SLEEP, ms, 0, 0);
 }
 
-uint32_t thread_create(void (*entry)(void *param), void *param, uint32_t pri)
+int noza_thread_create(void (*entry)(void *param), void *param, uint32_t pri)
 {
-    return noza_syscall(NSC_CREATE_THREAD, (uint32_t) entry, (uint32_t) param);
+    return noza_syscall(NSC_CREATE_THREAD, (uint32_t) entry, (uint32_t) param, pri);
 }
 
-uint32_t thread_yield()
+int noza_thread_yield()
 {
-	return noza_syscall(NSC_YIELD, 0, 0);
+	return noza_syscall(NSC_YIELD, 0, 0, 0);
+}
+
+int noza_recv(noza_port_t *port, noza_msg_t *msg)
+{
+	return 0;
+}
+
+int noza_reply(noza_port_t *port, uint32_t reply_code)
+{
+	return 0;
+}
+
+int noza_call(noza_port_t *port, noza_msg_t *msg)
+{
+	return 0;
+}
+
+int noza_nonblock_call(noza_port_t *port, noza_msg_t *msg)
+{
+	return 0;
+}
+
+int noza_nonblock_recv(noza_port_t *port, noza_msg_t *msg)
+{
+	return 0;
 }
