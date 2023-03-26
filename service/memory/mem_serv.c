4ea97e69d6a2d88b9109f6d19a6d0c5d2729c25a
Kevin Huang <hkt999@gmail.com>
2023-03-26 22:35:51 +0800

    first working dual core SMP

--- service/memory/mem_serv.c
+++ service/memory/mem_serv.c
@@ -1,5 +1,3 @@
-#if 0
-
 #include <sys/types.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -15,8 +13,6 @@ void *_sbrk(ptrdiff_t increment)
     char *prev_heap_end = heap_end;
     char *new_heap_end = heap_end + increment;
 
-    printf("------------------ %p, %p, %d\n", heap_end, heap_limit, (int)((char *)heap_limit - (char *)heap_end));
-
     if (new_heap_end > (char *)heap_limit) {
         errno = ENOMEM;
         return (void *)-1;
@@ -25,33 +21,3 @@ void *_sbrk(ptrdiff_t increment)
     heap_end = new_heap_end;
     return (void *)prev_heap_end;
 }
-
-void *_malloc_r(struct _reent *reent_ptr, size_t size)
-{
-    void *p = _sbrk(size);
-    printf("************---- malloc ************---- %p\n", p);
-    printf("SSSS\n");
-    return p;
-}
-
-void *_calloc_r(struct _reent *reent_ptr, size_t num, size_t size)
-{
-    printf("************---- calloc ************----\n");
-    void *p = _sbrk(num * size);
-    memset(p, 0, num * size);
-    return p;
-}
-
-void *_realloc_r(struct _reent *reent_ptr, void *ptr, size_t size)
-{
-    printf("************---- realloc ************----\n");
-    void *p = _sbrk(size);
-    memcpy(p, ptr, size);
-    return p;
-}
-
-void _free_r(struct _reent *reent_ptr, void *ptr)
-{
-    printf("************---- free ************---- %p, %p\n", reent_ptr, ptr);
-}
-#endif
