51889c5513c0e746b781173a8555f7c0ad5ca44d
Kevin Huang <hkt999@gmail.com>
2023-03-31 14:38:07 +0800

    move application configuration to cmake

--- user/noza_os_demo.c
+++ user/noza_os_demo.c
@@ -3,22 +3,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include "nozaos.h"
-
 #include "user/console/noza_console.h"
 
-#ifdef NOZAOS_TFCMSIS_DEMO_H
-extern int hello_world_main(int argc, char **argv);
-extern int magic_wand_main(int argc, char **argv);
-extern int micro_speech_main(int argc, char **argv);
-extern int person_detection_main(int argc, char **argv);
-builtin_cmd_t builtin_cmds[] = {
-    {"hello_world", hello_world_main, "tensorflow lite demo -- hello world"},
-    {"magic_wand", magic_wand_main, "tensorflow lite demo -- magic wand"},
-    {"micro_speech", micro_speech_main, "tensorflow lite demo -- micro speech"},
-     {"person_detection", person_detection_main, "tensorflow lite demo -- person detection"},
-    {NULL, NULL}
-};
+#define MAX_BUILTIN_CMDS  16
+typedef struct {
+    int count;
+    builtin_cmd_t builtin_cmds[MAX_BUILTIN_CMDS];
+} builtin_table_t;
 
+static void builtin_add(builtin_table_t *table, const char *name, int (*main)(int argc, char **argv), const char *desc)
+{
+    if (table->count >= MAX_BUILTIN_CMDS-2) {
+        return; // fail
+    }
+    table->builtin_cmds[table->count].name = name;
+    table->builtin_cmds[table->count].main_func = main;
+    table->builtin_cmds[table->count].help_msg = desc;
+    table->count++;
+}
+
+#ifdef NOZAOS_TFCMSIS
 extern void RegisterDebugLogCallback(void (*cb)(const char* s));
 void tflm_debug(const char *s)
 {
@@ -26,34 +30,40 @@ void tflm_debug(const char *s)
 }
 #endif
 
+void __user_start()
+{
+    builtin_table_t table;
+    memset(&table, 0, sizeof(table));
+
+#ifdef NOZAOS_TFCMSIS
+    extern int hello_world_main(int argc, char **argv);
+    extern int magic_wand_main(int argc, char **argv);
+    extern int micro_speech_main(int argc, char **argv);
+    extern int person_detection_main(int argc, char **argv);
+    extern void RegisterDebugLogCallback(void (*cb)(const char* s));
+    RegisterDebugLogCallback(tflm_debug);
+
+    builtin_add(&table, "hello_world", hello_world_main, "tensorflow lite demo -- hello world");
+    builtin_add(&table, "magic_wand", magic_wand_main, "tensorflow lite demo -- magic wand");
+    builtin_add(&table, "micro_speech", micro_speech_main, "tensorflow lite demo -- micro speech");
+    builtin_add(&table, "person_detection", person_detection_main, "tensorflow lite demo -- person detection");
+#endif
+
 #ifdef NOZAOS_UNITTEST
-extern int task_test(int argc, char **argv);
-extern int message_test(int argc, char **argv);
-extern int thread_join_test(int argc, char **argv);
-builtin_cmd_t builtin_cmds[] = {
-    {"test_task", task_test, "a demo program for task creation and scheduling"},
-    {"test_msg", message_test, "a demo program for message passing"},
-    {"test_join", thread_join_test, "a demo program for thread synchronization join"},
-    {NULL, NULL}
-};
+    extern int task_test(int argc, char **argv);
+    extern int message_test(int argc, char **argv);
+    extern int thread_join_test(int argc, char **argv);
+    builtin_add(&table, "test_task", task_test, "a demo program for task creation and scheduling");
+    builtin_add(&table, "test_msg", message_test, "a demo program for message passing");
+    builtin_add(&table, "test_join", thread_join_test, "a demo program for thread synchronization join");
 #endif
 
 #ifdef NOZAOS_LUA
-extern int lua_main(int argc, char **argv);
-builtin_cmd_t builtin_cmds[] = {
-    {"test_task", task_demo, "a demo program for task creation and scheduling"},
-    {"test_msg", message_demo, "a demo program for message passing"},
-    {"test_join", thread_join_demo, "a demo program for thread synchronization join"},
-    {"lua", lua_main, "lua interpreter"},
-    {NULL, NULL}
-};
+    extern int lua_main(int argc, char **argv);
+    builtin_add(&table, "lua", lua_main, "run Lua interpreter");
 #endif
-
-
-void __user_start()
-{
-    // RegisterDebugLogCallback(tflm_debug);
-    uint32_t th = noza_thread_create(console_start, &builtin_cmds[0], 0);
+    
+    uint32_t th = noza_thread_create(console_start, &table.builtin_cmds[0], 0);
 
     noza_thread_join(th);
     noza_thread_terminate();
