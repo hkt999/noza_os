bc6b746b1084123ff8a0a33fd7f87c266c01c544
Kevin Huang <hkt999@gmail.com>
2023-03-31 16:30:25 +0800

    implement setjmp/longjmp and test program

--- user/libc/include/setjmp.h
+++ user/libc/include/setjmp.h
@@ -1,16 +1,11 @@
 #pragma once
 
-struct _jmp_buf {
-	uint32_t sp;    // Stack pointer
-	uint32_t r4;
-	uint32_t r5;
-	uint32_t r6;
-	uint32_t r7;
-	uint32_t lr;    // Link register
-};
+#include <stdint.h>
+
+typedef uint32_t jmp_buf[11];
 
 int		setjmp(jmp_buf env);
 void	longjmp(jmp_buf env, int val);
 
 #define _setjmp(e)		setjmp(e)
-#define _longjmp(e,v)	longjmp(e,v)
+#define _longjmp(e,v)	longjmp(e, v)
