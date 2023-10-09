d92869b77f8782b262241683a505afef8bc80ffa
Kevin Huang <hkt999@gmail.com>
2023-10-09 12:16:03 +0800

    refactory thread structure

--- user/libc/src/syslib.c
+++ user/libc/src/syslib.c
@@ -2,24 +2,25 @@
 #include <stdlib.h> // TODO: remove malloc and free to use the memory service
 #include "nozaos.h"
 #include "kernel/syscall.h"
+#include "kernel/noza_config.h"
+
+#define NO_AUTO_FREE_STACK	0
+#define AUTO_FREE_STACK		1
 
 extern int noza_syscall(uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3);
 
-typedef struct boot_info {
+typedef struct thread_info {
 	int (*user_entry)(void *param, uint32_t pid);
 	void *user_param;
 	uint32_t *stack_ptr;
 	uint32_t stack_size;
+	uint32_t priority;
 	uint32_t need_free_stack;
 	uint32_t pid;
-} boot_info_t;
+} thread_info_t;
+static thread_info_t *THREAD_INFO[NOZA_OS_TASK_LIMIT] = {0};
 
-void free_stack(void *ptr)
-{
-	free(ptr);
-}
-
-extern void app_run(boot_info_t *info);
+extern void app_run(thread_info_t *info);
 
 #define MAX_SERVICES    8
 static void *service_entry[MAX_SERVICES];
@@ -43,24 +44,44 @@ void noza_run_services()
     }
 }
 
-#define NO_AUTO_FREE_STACK	0
-#define AUTO_FREE_STACK	1
+void free_stack(uint32_t pid)
+{
+	// TODO: consider free before set (because of thread priority)
+	// sanity check
+	if (pid >= NOZA_OS_TASK_LIMIT) {
+		printf("fatal: free_stack: invalid pid: %ld\n", pid);
+		return;
+	}
+	if (THREAD_INFO[pid] == NULL) {
+		printf("fatal: free_stack: pid %ld not found\n", pid);
+		return;
+	}
+	if (THREAD_INFO[pid]->stack_ptr == NULL) {
+		printf("fatal: free_stack: pid %ld stack_ptr is NULL\n", pid);
+		return;
+	}
+	// free user level stack
+	if (THREAD_INFO[pid]->need_free_stack == AUTO_FREE_STACK) {
+		free(THREAD_INFO[pid]->stack_ptr);
+	}
+	THREAD_INFO[pid] = NULL; // clear
+}
+
 int noza_thread_create_with_stack(uint32_t *pth, int (*entry)(void *, uint32_t pid), void *param, uint32_t priority, void *user_stack, uint32_t size, uint32_t auto_free_stack)
 {
-	volatile boot_info_t boot_info;
-	boot_info.user_entry = entry;
-	boot_info.user_param = param;
-	boot_info.stack_ptr = (uint32_t *)user_stack;
-	boot_info.stack_size = size;
-	boot_info.need_free_stack = auto_free_stack;
-	boot_info.pid = -1;
-    int ret =  noza_syscall(NSC_THREAD_CREATE, (uint32_t) app_run, (uint32_t) &boot_info, priority);
-	if (ret == 0) {
-		while (boot_info.pid==-1) {
-			noza_thread_yield(); // TODO: check if the thread is higher then child, then this never happen !!
-		}
-		*pth = boot_info.pid;
-	}
+	thread_info_t *thread_info = (thread_info_t *)user_stack; // stack tail for temporary use
+	thread_info->user_entry = entry;
+	thread_info->user_param = param;
+	thread_info->stack_ptr = (uint32_t *)user_stack;
+	thread_info->stack_size = size;
+	thread_info->need_free_stack = auto_free_stack;
+	int ret, th;
+    noza_syscall(NSC_THREAD_CREATE, (uint32_t) app_run, (uint32_t)thread_info, priority);
+	asm volatile("mov %0, r0" : "=r"(ret) : : "memory");
+	asm volatile("mov %0, r1" : "=r"(th) : : "memory");
+	thread_info->pid = th;
+	*pth = th;
+	THREAD_INFO[th] = thread_info;
 
 	return ret;
 }
