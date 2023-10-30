944a2ab449e6719bbe40bddf1521db25f5b24176
Kevin Huang <hkt999@gmail.com>
2023-10-30 21:54:41 +0800

    remove noza_thread_self() system call and implement new noza_thread_self() without traping into kernel

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -8,12 +8,11 @@
 #define NSC_THREAD_JOIN                 4
 #define NSC_THREAD_DETACH               5
 #define NSC_THREAD_TERMINATE            6
-#define NSC_THREAD_SELF                 7
 // IPC
-#define NSC_RECV                        8
-#define NSC_REPLY                       9
-#define NSC_CALL                        10
-#define NSC_NB_RECV                     11
-#define NSC_NB_CALL                     12
-#define NSC_NUM_SYSCALLS                13
+#define NSC_RECV                        7
+#define NSC_REPLY                       8
+#define NSC_CALL                        9
+#define NSC_NB_RECV                     10
+#define NSC_NB_CALL                     11
+#define NSC_NUM_SYSCALLS                12
 
