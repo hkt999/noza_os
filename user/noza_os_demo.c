aa3be59dee30136f0a9266770cf55f26c46f3c18
Kevin Huang <hkt999@gmail.com>
2023-10-30 18:25:19 +0800

    merge, change name from thread_info to thread_record

--- user/noza_os_demo.c
+++ user/noza_os_demo.c
@@ -5,14 +5,8 @@
 #include "nozaos.h"
 #include "user/console/noza_console.h"
 
+_Thread_local int threadLocalVar = 0;
 void root_task(void *param)
 {
-    #if 0
-    uint32_t th;
-    noza_thread_create(&th, console_start, NULL, 0, 4096);
-    noza_thread_join(th, NULL);
-    #else
     console_start();
-    //console_start(&table.builtin_cmds[0], 0);
-    #endif
 }
