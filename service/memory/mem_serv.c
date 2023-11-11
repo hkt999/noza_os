49a67ff8408c09c99e8174653e8c8d8be42a4855
Kevin Huang <hkt999@gmail.com>
2023-11-11 22:14:22 +0800

    add tinyalloc for memory management

--- service/memory/mem_serv.c
+++ service/memory/mem_serv.c
@@ -23,14 +23,40 @@ void *_sbrk(ptrdiff_t increment)
     heap_end = new_heap_end;
     return (void *)prev_heap_end;
 }
+
+Argument	Description
+base	Address of tinyalloc control structure, typically at the beginning of your heap
+limit	Heap space end address
+heap_blocks	Max. number of memory chunks (e.g. 256)
+split_thresh	Size threshold for splitting chunks (a good default is 16)
+alignment	Word size for pointer alignment (e.g. 8)
+alignment is assumed to be >= native word size
+base must be an address in RAM (on embedded devices)
+
+
 #endif
 
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
 #include "mem_serv.h"
 #include "nozaos.h"
+#include "3rd_party/tinyalloc/tinyalloc.h"
+
+extern char __end__, __StackLimit; // __StackLimit is poorly named, it's actually the end of the heap
+void *heap_end = &__end__;
+void *heap_limit = &__StackLimit;
+
+void *_sbrk(ptrdiff_t increment)
+{
+    char *prev_heap_end = heap_end;
+    char *new_heap_end = heap_end + increment;
+
+    if (new_heap_end > (char *)heap_limit) {
+        //errno = ENOMEM;
+        return (void *)-1;
+    }
+
+    heap_end = new_heap_end;
+    return (void *)prev_heap_end;
+}
 
 uint32_t memory_pid = 0;
 static int do_memory_server(void *param, uint32_t pid)
@@ -38,14 +64,17 @@ static int do_memory_server(void *param, uint32_t pid)
     int ret;
 	memory_pid = pid;
     noza_msg_t msg;
-
+    
+    void *heap = _sbrk(100*1024);
+    ta_init(heap, heap + 100*1024, 256, 16, 8);
     for (;;) {
         if ((ret = noza_recv(&msg)) == 0) { // the pid in msg is the sender pid
 			mem_msg_t *mem_msg = (mem_msg_t *)msg.ptr;
 			// process the request
 			switch (mem_msg->cmd) {
 				case MEMORY_MALLOC:
-                    mem_msg->ptr = malloc(mem_msg->size);
+                    //mem_msg->ptr = malloc(mem_msg->size);
+                    mem_msg->ptr = ta_alloc(mem_msg->size);
                     if (mem_msg->ptr == NULL) {
                         mem_msg->code = MEMORY_INVALID_OP;
                     } else {
@@ -54,7 +83,8 @@ static int do_memory_server(void *param, uint32_t pid)
 					break;
 
 				case MEMORY_FREE:
-                    free(mem_msg->ptr);
+                    //free(mem_msg->ptr);
+                    ta_free(mem_msg->ptr);
                     mem_msg->ptr = NULL;
                     mem_msg->code = MEMORY_SUCCESS;
                     break;
@@ -74,7 +104,6 @@ static int do_memory_server(void *param, uint32_t pid)
 static uint8_t memory_server_stack[1024];
 void __attribute__((constructor(110))) memory_server_init(void *param, uint32_t pid)
 {
-    // TODO: move the external declaraction into a header file
     extern void noza_add_service(int (*entry)(void *param, uint32_t pid), void *stack, uint32_t stack_size);
 	noza_add_service(do_memory_server, memory_server_stack, sizeof(memory_server_stack));
 }
