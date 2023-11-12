7ff57b13aa2439282455ebcfc0ee084bdb50fa1a
Kevin Huang <hkt999@gmail.com>
2023-11-12 12:39:52 +0800

    change memory service to tinyalloc

--- service/memory/mem_serv.c
+++ service/memory/mem_serv.c
@@ -1,43 +1,6 @@
-// TODO: use inhouse malloc/free instead of the system one
-#if 0
-#include <sys/types.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-extern char __end__, __StackLimit; // __StackLimit is poorly named, it's actually the end of the heap
-void *heap_end = &__end__;
-void *heap_limit = &__StackLimit;
-
-void *_sbrk(ptrdiff_t increment)
-{
-    char *prev_heap_end = heap_end;
-    char *new_heap_end = heap_end + increment;
-
-    if (new_heap_end > (char *)heap_limit) {
-        errno = ENOMEM;
-        return (void *)-1;
-    }
-
-    heap_end = new_heap_end;
-    return (void *)prev_heap_end;
-}
-
-Argument	Description
-base	Address of tinyalloc control structure, typically at the beginning of your heap
-limit	Heap space end address
-heap_blocks	Max. number of memory chunks (e.g. 256)
-split_thresh	Size threshold for splitting chunks (a good default is 16)
-alignment	Word size for pointer alignment (e.g. 8)
-alignment is assumed to be >= native word size
-base must be an address in RAM (on embedded devices)
-
-
-#endif
-
 #include "mem_serv.h"
 #include "nozaos.h"
+#include "posix/errno.h"
 #include "3rd_party/tinyalloc/tinyalloc.h"
 
 extern char __end__, __StackLimit; // __StackLimit is poorly named, it's actually the end of the heap
@@ -50,7 +13,6 @@ void *_sbrk(ptrdiff_t increment)
     char *new_heap_end = heap_end + increment;
 
     if (new_heap_end > (char *)heap_limit) {
-        //errno = ENOMEM;
         return (void *)-1;
     }
 
@@ -58,6 +20,7 @@ void *_sbrk(ptrdiff_t increment)
     return (void *)prev_heap_end;
 }
 
+#include <stdio.h>
 uint32_t memory_pid = 0;
 static tinyalloc_t tinyalloc;
 static int do_memory_server(void *param, uint32_t pid)
@@ -65,25 +28,24 @@ static int do_memory_server(void *param, uint32_t pid)
     int ret;
 	memory_pid = pid;
     noza_msg_t msg;
-    void *heap = _sbrk(100*1024);
-    ta_init(&tinyalloc, heap, heap + 100*1024, 256, 16, 8);
+
+    ta_init(&tinyalloc, heap_end, heap_limit, 256, 16, 8);
+    heap_end = heap_limit;
     for (;;) {
         if ((ret = noza_recv(&msg)) == 0) { // the pid in msg is the sender pid
 			mem_msg_t *mem_msg = (mem_msg_t *)msg.ptr;
 			// process the request
 			switch (mem_msg->cmd) {
 				case MEMORY_MALLOC:
-                    //mem_msg->ptr = malloc(mem_msg->size);
                     mem_msg->ptr = ta_alloc(&tinyalloc, mem_msg->size);
                     if (mem_msg->ptr == NULL) {
-                        mem_msg->code = MEMORY_INVALID_OP;
+                        mem_msg->code = ENOMEM;
                     } else {
                         mem_msg->code = MEMORY_SUCCESS;
                     }
 					break;
 
 				case MEMORY_FREE:
-                    //free(mem_msg->ptr);
                     ta_free(&tinyalloc, mem_msg->ptr);
                     mem_msg->ptr = NULL;
                     mem_msg->code = MEMORY_SUCCESS;
