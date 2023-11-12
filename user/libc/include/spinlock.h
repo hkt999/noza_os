b5181a481f6a96d983923d2a8c0159b96440be03
Kevin Huang <hkt999@gmail.com>
2023-11-13 01:47:13 +0800

    add noza spinlock support

---
+++ user/libc/include/spinlock.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct spinlock_s {
+    int num;
+    volatile uint32_t *spinlock;
+    int lock_thread;
+} spinlock_t;
+
+int noza_spinlock_init(spinlock_t *spinlock);
+int noza_spinlock_free(spinlock_t *spinlock);
+int noza_spinlock_lock(spinlock_t *spinlock);
+int noza_spinlock_trylock(spinlock_t *spinlock);
+int noza_spinlock_unlock(spinlock_t *spinlock);
---
+++ user/libc/src/spinlock.c
@@ -0,0 +1,55 @@
+#include "spinlock.h"
+#include "pico.h"
+#include "hardware/address_mapped.h"
+#include "hardware/sync.h"
+#include "hardware/regs/sio.h"
+#include "nozaos.h"
+#include "posix/errno.h"
+
+// hardware spinlock interface
+int noza_spinlock_init(spinlock_t *spinlock) {
+    spinlock->num = spin_lock_claim_unused(true);
+    spinlock->spinlock = spin_lock_init(spinlock->num); // init spin lock
+    spinlock->lock_thread = -1;
+    return 0; // success
+}
+
+int noza_spinlock_free(spinlock_t *spinlock) {
+    spin_lock_unclaim(spinlock->num);
+    return 0; // success
+}
+
+int noza_spinlock_lock(spinlock_t *spinlock) {
+    uint32_t tid;
+    noza_thread_self(&tid);
+    if (spinlock->lock_thread == tid)
+        return EDEADLK; // deadlock
+
+    while (is_spin_locked(spinlock->spinlock)) {
+        noza_thread_sleep_us(0, NULL); // yield
+    }
+    spin_lock_unsafe_blocking (spinlock->spinlock);
+    spinlock->lock_thread = tid;
+
+    return 0;
+}
+
+int noza_spinlock_trylock(spinlock_t *spinlock) {
+    uint32_t tid;
+    noza_thread_self(&tid);
+    if (spinlock->lock_thread == tid)
+        return EDEADLK; // deadlock
+
+    if (is_spin_locked(spinlock->spinlock))
+        return EBUSY; // already locked
+
+    spin_lock_unsafe_blocking(spinlock->spinlock); // acquire lock
+    spinlock->lock_thread = tid;
+    return 0;
+}
+
+int noza_spinlock_unlock(spinlock_t *spinlock) {
+    spin_unlock_unsafe(spinlock->spinlock);
+    spinlock->lock_thread = -1;
+    return 0;
+}
